f8e4b71c462facd04a82e97cfb756039ed0b274e
erica-r05 <reginaerica073@gmail.com>
2025-10-10 19:21:53 -0300

    incorporação do bootstrap no carrossel

--- app.js
+++ app.js
@@ -1,30 +1,3 @@
-let currentSlide = 0;
-const slides = document.querySelectorAll('.slide');
-const carrossel = document.getElementById('carrossel');
-
-function mudarSlide(direction) {
-    slides[currentSlide].classList.remove('active');
-    
-    currentSlide = (currentSlide + direction + slides.length) % slides.length;
-    
-    slides[currentSlide].classList.add('active');
-    const slideWidth = carrossel.offsetWidth;
-    carrossel.scrollTo({
-        left: currentSlide * slideWidth,
-        behavior: 'smooth'
-    });
-}
-
-function irParaSlide(n) {
-    const slideIndex = n - 1;
-    if (slideIndex === currentSlide) return;
-
-    slides[currentSlide].classList.remove('active');
-    currentSlide = slideIndex;
-    slides[currentSlide].classList.add('active');
-    const slideWidth = carrossel.offsetWidth;
-    carrossel.scrollTo({
-        left: currentSlide * slideWidth,
-        behavior: 'smooth'
-    });
+function irParaDetalhe(itemId) {
+    window.location.href = "detalhes.html?id=" + itemId;
 }
